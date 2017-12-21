091c044470a0cb4eca18c6588935f8bbd830b2ab
MrTyton <joshua.gang@gmail.com>
2017-12-21 14:37:43 -0500

    setup files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'ConvergentRandomChoice',
+  packages = ['ConvergentRandomChoice'],
+  version = '0.1',
+  description = 'Convergent Random Walk Choice lib.',
+  author = 'Joshua Gang',
+  author_email = 'joshua.gang@gmail.com',
+  url = 'https://github.com/MrTyton/ConvergentRandomChoice', 
+  download_url = 'https://github.com/MrTyton/ConvergentRandomChoice/archive/0.1.tar.gz', 
+  keywords = ['random walk', 'numpy',], 
+  install_requires = ['numpy']
+  classifiers = [],
+)
